4b82d855a9fdc24ee9e130dc3214cda76652d0e8
DGollings <2032823+DGollings@users.noreply.github.com>
2023-06-04 19:05:21 +0200

    Update go.nix

--- src/modules/languages/go.nix
+++ src/modules/languages/go.nix
@@ -27,6 +27,7 @@ in
 
       # Required by vscode-go
       pkgs.delve
+      pkgs.gcc
 
       # vscode-go expects all tool compiled with the same used go version, see: https://github.com/golang/vscode-go/blob/72249dc940e5b6ec97b08e6690a5f042644e2bb5/src/goInstallTools.ts#L721
       (buildWithSpecificGo pkgs.gotools)
